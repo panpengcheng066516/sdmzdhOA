9ed4b5b2b0ef576b62142b345c722aa6bc08b5c1
ppc <ppc066516@163.com>
2020-09-21 16:50:55 +0800

    补充修改

--- OfficeAutomation/src/com/zdh/dao/DesignWorkingDao.java
+++ OfficeAutomation/src/com/zdh/dao/DesignWorkingDao.java
@@ -9,21 +9,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class DesignWorkingDao {
-    //获取全部调试工作列表
+    //获取全部设计工作列表
     public List<DesignWorking> getDesignWorkingList() throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "select * from Design";
         return runner.query(sql, new BeanListHandler<DesignWorking>(DesignWorking.class));
     }
 
-    //通过用户名获取调试工作列表
+    //通过用户名获取设计工作列表
     public List<DesignWorking> getDesignWorkingByUsername(String username) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "select * from Design where username = ?";
         return runner.query(sql, new BeanListHandler<DesignWorking>(DesignWorking.class),username);
     }
 
-    //新增调试工作
+    //新增设计工作
     public int addDesignWorking(DesignWorking designWorking) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "insert into Design (id,year,month,username,amount,a1,zheheWorkingDay,monthDay,programDay,BasicDesignDay,leader,remark,projectid) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
@@ -32,17 +32,17 @@ public class DesignWorkingDao {
                 designWorking.getProgramDay(),designWorking.getBasicDesignDay(),designWorking.getLeader(),designWorking.getRemark(),designWorking.getPeojectid());
     }
 
-    //通过id删除调试工作
+    //通过id删除设计工作
     public int deleteDesignWorkingByid(String id) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "delete from Design where id = ?";
         return runner.update(sql,id);
     }
 
-    //更新调试工作
+    //更新设计工作
     public int updateDesignWorking(DesignWorking designWorking) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
-        String sql = "update Debug set amount = ?,a1 = ?,zheheWorkingDay = ?,monthDay = ?,programDay = ?,basicDesignDay = ?,leader = ?,remark = ?,projectid = ?  where id = ?";
+        String sql = "update Design set amount = ?,a1 = ?,zheheWorkingDay = ?,monthDay = ?,programDay = ?,basicDesignDay = ?,leader = ?,remark = ?,projectid = ?  where id = ?";
         int update = runner.update(sql,designWorking.getAmount(),designWorking.getA1(),designWorking.getZheheWorkingDay(),designWorking.getMonthDay(),
                 designWorking.getProgramDay(),designWorking.getBasicDesignDay(),designWorking.getLeader(),designWorking.getRemark(),designWorking.getPeojectid(),designWorking.getId());
         return update;
--- OfficeAutomation/src/com/zdh/dao/ManageWorkingDao.java
+++ OfficeAutomation/src/com/zdh/dao/ManageWorkingDao.java
@@ -9,21 +9,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ManageWorkingDao {
-    //获取全部调试工作列表
+    //获取全部管理工作列表
     public List<ManageWorking> getManageWorkingList() throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "select * from Manage";
         return runner.query(sql, new BeanListHandler<ManageWorking>(ManageWorking.class));
     }
 
-    //通过用户名获取调试工作列表
+    //通过用户名获取管理工作列表
     public List<ManageWorking> getManageWorkingByUsername(String username) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "select * from Manage where username = ?";
         return runner.query(sql, new BeanListHandler<ManageWorking>(ManageWorking.class),username);
     }
 
-    //新增调试工作
+    //新增管理工作
     public int addManageWorking(ManageWorking manageWorking) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "insert into Manage (year,month,username,id,xunjiabaojia,tender,sign,toubiao,equip,test,cuikuan,contract,other,PMday,remark,projectid) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
@@ -33,18 +33,20 @@ public class ManageWorkingDao {
                 manageWorking.getRemark(),manageWorking.getPeojectid());
     }
 
-    //通过id删除调试工作
+    //通过id删除管理工作
     public int deleteManageWorkingByid(String id) throws SQLException {
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "delete from Manage where id = ?";
         return runner.update(sql,id);
     }
 
-//    //更新调试工作
-//    public int updateManageWorking(ManageWorking manageWorking) throws SQLException {
-//        QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
-//        String sql = "update Manage set  where id = ?";
-//        int update = runner.update(sql,);
-//        return update;
-//    }
+    //更新管理工作
+    public int updateManageWorking(ManageWorking manageWorking) throws SQLException {
+        QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
+        String sql = "update Manage set year=?,month=?,xunjiabaojia=?,tender=?,sign=?,toubiao=?,equip=?,test=?,cuikuan=?,contract=?,other=?,PMday=?,remark=?projectid=? where id = ?";
+        int update = runner.update(sql,manageWorking.getYear(),manageWorking.getMonth(),manageWorking.getXunjiabaojia(),manageWorking.getTender(),
+                manageWorking.getSign(),manageWorking.getToubiao(),manageWorking.getEquip(),manageWorking.getTest(),manageWorking.getCuikuan(),
+                manageWorking.getContract(),manageWorking.getOther(),manageWorking.getPMday(),manageWorking.getRemark(),manageWorking.getPeojectid());
+        return update;
+    }
 }
